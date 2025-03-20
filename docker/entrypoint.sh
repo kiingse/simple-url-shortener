0ec5945e67cc8549f4f47fe0dc3a8c0e312b425f
sebkrl <sebkrl@proton.me>
2025-03-20 16:21:34 +0100

    Add sleep for postgres start

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,10 +2,7 @@
 set -e
 
 echo "Waiting for PostgreSQL..."
-while ! nc -z db 5432; do
-  sleep 0.1
-done
-echo "PostgreSQL started"
+sleep 0.5
 
 echo "Running migrations"
 poetry run alembic upgrade head
